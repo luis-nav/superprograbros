812929a7c1fba39215134f5e49772a22ff13cd56
luis-nav <luiscatodd@gmail.com>
2024-03-21 14:41:16 -0600

    feat: primera fase explorador

--- explorador/explorador.py
+++ explorador/explorador.py
@@ -1,4 +1,6 @@
+import re
 
+from explorador.ComponenteLexico import ComponenteLexico, TipoComponenteLexico
 
 class Explorador:
 
@@ -10,8 +12,34 @@ class Explorador:
         self.errores = []
 
     def __explorar(self):
+        counter = 1
         for linea in self.archivo:
-            resultado = self.__procesarLinea(linea)
+            resultado = self.__procesarLinea(linea, counter)
             self.componentes = self.componentes + resultado
+            counter += 1
 
-    def __procesarLinea(self, linea):
+    def __procesarLinea(self, linea, numeroLinea):
+        componentes = []
+    
+        while (linea != ""):
+
+            for tipoComponente, regex in self.descriptoresComponentes:
+
+                respuesta = re.match(regex, linea)
+
+                nuevoComponente = ComponenteLexico(respuesta.group(), tipoComponente, numeroLinea, respuesta.span[0])
+
+                # Si se encuentra algo diferente a un comentario o espacios agrega el componente lexico
+                if tipoComponente is not TipoComponenteLexico.COMENTARIO and tipoComponente is not TipoComponenteLexico.BLANCOS:
+                    componentes.append(nuevoComponente)
+
+                # Si no identifico ningun patron y la linea no esta vacia es un error
+                # Deja de explorar la linea actual y sigue con la siguiente para buscar mas errores
+                if tipoComponente is TipoComponenteLexico.NO_IDENTIFICADO:
+                    self.errores.append(nuevoComponente)
+                    return componentes
+
+                linea = linea[respuesta.end():]
+                break;
+
+        return componentes
