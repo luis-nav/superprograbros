b33e56be3fe65f38ab5554e5efbd18ae19518ed7
Luis-Nav <luiscatodd@gmail.com>
2024-05-26 13:23:42 -0600

    añadir: funciones al ambiente estandar: aplastarGoomba, bloque, wario, pausa

--- generador/generador.py
+++ generador/generador.py
@@ -9,6 +9,7 @@ class Generador:
 
     ambienteEstandar = """import sys
 import random
+import time
 
 def champiñon(txt1, txt2):
     return txt1 + txt2
@@ -34,6 +35,19 @@ def yoshi(flt):
 
 def daisy(caracter):
     return caracter.lower()
+
+def aplastarGoomba(txt, index, caracter):
+    txt = txt[:index] + caracter + txt[index+1:]
+    return txt
+
+def bloque(inicio, fin):
+    return random.randint(inicio,fin)
+
+def wario(txt, caracter):
+    return txt.index(caracter)
+
+def pausa(segundos):
+    time.sleep(segundos)
 """
 
     def __init__(self, asa: ArbolSintaxisAbstracta) -> None:
